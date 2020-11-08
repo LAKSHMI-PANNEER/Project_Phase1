8b9d02519a3c10330ad6cf885bf828e1bc885f97
Lakshmi-P <lakshmipanneer08@gmail.com>
2020-11-08 15:16:42 +0530

    module-1

---
+++ url.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url ='https://support.google.com/edu/classroom/answer/7080036?hl=en&ref_topic=9050121'
+
+reqs = requests.get(url)
+
+soup = BeautifulSoup(reqs.text,'html.parser')
+
+for tag in soup.find_all("ol"): 
+    #print("{0}: {1}".format(tag.name, tag.text))
+    t=tag.text
+    print(t.strip())
+
+
