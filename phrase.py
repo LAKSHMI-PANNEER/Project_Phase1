35c9c95c7f736935cce703f64f4e99fd820641db
Lakshmi-P <lakshmipanneer08@gmail.com>
2020-11-08 18:09:55 +0530

    module-1

---
+++ phrase.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+import nltk
+import string
+from nltk import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+url ='https://support.google.com/edu/classroom/answer/7080036?hl=en&ref_topic=9050121'
+
+reqs = requests.get(url)
+
+soup = BeautifulSoup(reqs.text,'html.parser')
+
+for tag in soup.find_all("ol"): 
+    #print("{0}: {1}".format(tag.name, tag.text))
+    t=tag.text
+    print(t)
+
+#sentence
+sentences = sent_tokenize(t)
+print(sentences[0],"\n")
+
+#words without punctuation
+tokens = word_tokenize(t)
+words = [word for word in tokens if word.isalpha()]
+print(words[:100],"\n")
+
+#removing stop words
+stop_words = set(stopwords.words('english'))
+words = [w for w in words if not w in stop_words]
+print(words[:100])
