ccdf624133ac014da5a0bcfa935101c437330f3e
Lakshmi-P <lakshmipanneer08@gmail.com>
2020-12-03 21:40:41 +0530

    phase-1

--- photos.py
+++ photos.py
@@ -7,15 +7,33 @@ from nltk.corpus import stopwords
 from nltk.probability import FreqDist
 from collections import Counter
 
-urls = ['https://www.lifewire.com/android-file-transfer-any-device-4173383',
-'https://www.lifewire.com/backup-android-phone-4177052',
-'https://www.lifewire.com/block-text-messages-every-phone-and-carrier-4172380',
-'https://www.lifewire.com/change-android-settings-4135692',
-'https://www.lifewire.com/change-google-assistant-voice-4169323',
-'https://www.lifewire.com/changing-android-ringtone-1616828',
-'https://www.lifewire.com/check-updates-for-android-1616953',
-'https://www.lifewire.com/clear-cache-android-4157780',
-'https://www.lifewire.com/close-apps-on-android-4164116'
+urls = ['https://support.google.com/photos/answer/6128838',
+'https://support.google.com/photos/answer/6128843?co=GENIE.Platform%3DAndroid&hl=en',
+'https://support.google.com/photos/answer/6128850',
+'https://support.google.com/photos/answer/6128858?visit_id=637213412787536629-872446421&rd=1',
+'https://support.google.com/photos/answer/6131416',
+'https://support.google.com/photos/answer/6131416?visit_id=637176625396538486-1031873361&hl=en&rd=1&co=GENIE.Platform%3DDesktop',
+'https://support.google.com/photos/answer/6153599?co=GENIE.Platform%3DAndroid&hl=en',
+'https://support.google.com/photos/answer/6193313?co=GENIE.Platform%3DAndroid&hl=en&oco=0',
+'https://support.google.com/photos/answer/6280921?hl=en',
+'https://support.google.com/photos/answer/7362432',
+'https://support.google.com/photos/answer/7378811?co=GENIE.Platform%3DAndroid&hl=en',
+'https://support.google.com/photos/answer/7378942?hl=en&ref_topic=7378810&co=GENIE.Platform%3DAndroid',
+'https://support.google.com/photos/answer/7539151?hl=en-GB&ref_topic=6128848',
+'https://support.google.com/photos/answer/9284827?hl=en&ref_topic=6156061',
+'https://support.google.com/photos/answer/9343965?hl=en',
+'https://support.google.com/photos/answer/9380189?co=GENIE.Platform%3DAndroid&hl=en',
+'https://support.google.com/photos/answer/9454489?hl=en-GB&ref_topic=6128848',
+'https://support.google.com/photos/answer/9454489?hl=ur&ref_topic=6128848',
+'https://support.google.com/photos/thread/29507038?hl=en',
+'https://www.androidcentral.com/how-set-google-photos',
+'https://www.lifewire.com/how-to-delete-google-photos-4690368',
+'https://www.lifewire.com/how-to-transfer-photos-from-phone-to-computer-4173057',
+'https://www.lifewire.com/how-to-frame-photo-like-polaroid-1701563',
+'https://www.lifewire.com/how-to-retrieve-google-backup-photos-4690013',
+'https://www.lifewire.com/recover-deleted-photos-on-android-4165361',
+'https://www.lifewire.com/save-instagram-photos-4125398',
+'https://www.wikihow.com/Assign-a-Photo-to-a-Contact-on-Your-Android-Phone'
 ]
 
 for url in urls:
